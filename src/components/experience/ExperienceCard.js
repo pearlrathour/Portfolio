7dcfabfbabc2d808b605cba2ab7b71a5cbbd06a8
Pearl <pearl.rathour33@gmail.com>
2024-05-31 20:47:17 +0530

    Modified Experience Section Cards

--- src/components/experience/ExperienceCard.js
+++ src/components/experience/ExperienceCard.js
@@ -1,91 +1,60 @@
-import React from 'react'
+import React, { useRef } from "react";
+import {
+    motion,
+    useMotionTemplate,
+    useMotionValue,
+    useSpring,
+} from "framer-motion";
 
 export default function ExperienceCard(props) {
+    const ROTATION_RANGE = 32.5;
+    const HALF_ROTATION_RANGE = 32.5 / 2;
+
+    const ref = useRef(null);
+    const x = useMotionValue(0);
+    const y = useMotionValue(0);
+    const xSpring = useSpring(x);
+    const ySpring = useSpring(y);
+    const transform = useMotionTemplate`rotateX(${xSpring}deg) rotateY(${ySpring}deg)`;
+
+    const handleMouseMove = (e) => {
+        if (!ref.current) return [0, 0];
+
+        const rect = ref.current.getBoundingClientRect();
+
+        const width = rect.width;
+        const height = rect.height;
+
+        const mouseX = (e.clientX - rect.left) * ROTATION_RANGE;
+        const mouseY = (e.clientY - rect.top) * ROTATION_RANGE;
+
+        const rX = (mouseY / height - HALF_ROTATION_RANGE) * -1;
+        const rY = mouseX / width - HALF_ROTATION_RANGE;
+
+        x.set(rX);
+        y.set(rY);
+    };
+
+    const handleMouseLeave = () => {
+        x.set(0);
+        y.set(0);
+    };
+
     return (
-        <a href={props.exp.link} className="flex flex-col shadow-lg shadow-gray-500 bg-blue-100 mx-auto w-1/3 py-10 transform duration-500 hover:-translate-y-4 cursor-pointer max-h-190 rounded-md">
-            <div>
-                <img className="mx-auto h-36 w-36" src={props.exp.img} alt="" />
-            </div>
-            <div className="font-bold text-xl mt-8 mb-4 text-gray-800 flex justify-center">{props.exp.position}</div>
-            <div className='flex flex-row justify-between mb-4 text-gray-800 px-4'>
-                <div className='font-semibold'>{props.exp.company}</div>
-                <div className='font-medium'>{props.exp.date}</div>
-            </div>
-            <p className="text-sm leading-relaxed text-gray-700 mt-6 px-5">
-                {props.exp.desc}
-            </p>
-
-            {/* <div className="flex flex-col shadow-xl mx-auto max-w-sm bg-red-100 py-20 px-12 transform duration-500 hover:-translate-y-2 cursor-pointer max-h-190 rounded-md">
-                <div className="min-h-62">
-                    <img className="mx-auto" src={GSSoC} alt="" />
+        <div className="grid w-full place-content-center px-4 py-12">
+            <motion.div ref={ref} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} style={{ transformStyle: "preserve-3d", transform, }}
+                className="relative h-96 w-72 rounded-xl border-2 border-cyan-800">
+                <div className="absolute inset-4 grid place-content-center rounded-xl bg-gradient-to-br from-cyan-400 to-slate-800 shadow-lg border-2 border-cyan-600" style={{ transform: "translateZ(75px)", transformStyle: "preserve-3d", }}>
+                    <div className="flex flex-col justify-evenly text-center text-gray-100" style={{ transform: "translateZ(50px)", }}>
+                        <img class="object-fill w-full h-56 px-4 transform duration-700 backdrop-opacity-100" src={props.exp.img} alt='Experience' />
+                        <div className="py-[4%]">
+                            <div className="text-xl font-bold">{props.exp.company}</div>
+                            <div className="text-sm font-thin">{props.exp.position}</div>
+                            <div className="text-sm font-thin py-[1%]">{props.exp.date}</div>
+                        </div>
+                    </div>
                 </div>
-                <h1 className="font-extrabold text-6xl mt-28 mb-10 text-gray-800">01.</h1>
-                <h2 className="font-bold mb-5 text-gray-800">Stylish Egg Chair</h2>
-                <p className="text-sm leading-relaxed text-gray-700">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id beatae repellendus nam! Dolor dignissimos unde, dolore laboriosam atque numquam quam.
-                </p>
-            </div>
-
-            <div className="flex flex-col shadow-xl mx-auto max-w-sm bg-yellow-100 py-20 px-12 transform duration-500 hover:-translate-y-2 cursor-pointer mt-0 md:mt-20 max-h-190 rounded-md">
-                <div className="min-h-62">
-                    <img className="mx-auto" src={SSoC} alt="" />
-                </div>
-                <h1 className="font-extrabold text-6xl mt-28 mb-10 text-gray-800">02.</h1>
-                <h2 className="font-bold mb-5 text-gray-800">Stylish Leather Bag</h2>
-                <p className="text-sm leading-relaxed text-gray-700">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id beatae repellendus nam! Dolor dignissimos unde, dolore laboriosam atque numquam quam.
-                </p>
-            </div>
-
-            <div className="flex flex-col shadow-xl mx-auto max-w-sm bg-blue-100 py-20 px-12 transform duration-500 hover:-translate-y-2 cursor-pointer max-h-190 rounded-md">
-                <div className="min-h-62">
-                    <img className="mx-auto" src={Hacktoberfest} alt="" />
-                </div>
-                <h1 className="font-extrabold text-6xl mt-28 mb-10 text-gray-800">03.</h1>
-                <h2 className="font-bold mb-5 text-gray-800">Modern Wooden Chair</h2>
-                <p className="text-sm leading-relaxed text-gray-700">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id beatae repellendus nam! Dolor dignissimos unde, dolore laboriosam atque numquam quam.
-                </p>
-            </div>
-
-            <div className="flex flex-col shadow-xl mx-auto max-w-sm bg-purple-100 py-20 px-12 transform duration-500 hover:-translate-y-2 cursor-pointer mt-0 md:mt-20 max-h-190 rounded-md">
-                <div className="min-h-62">
-                    <img className="mx-auto" src={CodessCafe} alt="" />
-                </div>
-                <h1 className="font-extrabold text-6xl mt-28 mb-10 text-gray-800">04.</h1>
-                <h2 className="font-bold mb-5 text-gray-800">Comfortable Chair</h2>
-                <p className="text-sm leading-relaxed text-gray-700">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id beatae repellendus nam! Dolor dignissimos unde, dolore laboriosam atque numquam quam.
-                </p>
-            </div>
-
-            <div className="flex flex-col shadow-xl mx-auto max-w-sm bg-purple-100 py-20 px-12 transform duration-500 hover:-translate-y-2 cursor-pointer mt-0 md:mt-20 max-h-190 rounded-md">
-                <div className="min-h-62">
-                    <img className="mx-auto" src={SPEC} alt="" />
-                </div>
-                <h1 className="font-extrabold text-6xl mt-28 mb-10 text-gray-800">04.</h1>
-                <h2 className="font-bold mb-5 text-gray-800">Comfortable Chair</h2>
-                <p className="text-sm leading-relaxed text-gray-700">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id beatae repellendus nam! Dolor dignissimos unde, dolore laboriosam atque numquam quam.
-                </p>
-            </div>
-
-            <div className="flex flex-col shadow-xl mx-auto max-w-sm bg-purple-100 py-20 px-12 transform duration-500 hover:-translate-y-2 cursor-pointer mt-0 md:mt-20 max-h-190 rounded-md">
-                <div className="min-h-62">
-                    <img className="mx-auto" src={Vibhav} alt="" />
-                </div>
-                <h1 className="font-extrabold text-6xl mt-28 mb-10 text-gray-800">04.</h1>
-                <h2 className="font-bold mb-5 text-gray-800">Comfortable Chair</h2>
-                <p className="text-sm leading-relaxed text-gray-700">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id beatae repellendus nam! Dolor dignissimos unde, dolore laboriosam atque numquam quam.
-                </p>
-            </div> */}
-
-
-        </a>
-    )
-}
-
-
-
-
+            </motion.div>
+        </div>
+    );
+};
